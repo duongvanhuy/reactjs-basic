427347c16d16f2e4a6f50b0e09edd1220baea3a4
duongvanhuy <duongvanhuy137@gmail.com>
2022-04-03 19:52:58 +0700

    fix: move function from parent to child

--- src/views/example/FormComponent.js
+++ src/views/example/FormComponent.js
@@ -18,24 +18,32 @@ class FormComponent extends React.Component {
     }
     handleOnClickSubmit = (event) => {
         event.preventDefault()
+        if (!this.state.title || !this.state.salary) {
+            alert('Missing required params')
+            return
+        }
         this.props.addComponent({
             id: Math.floor(Math.random() * 1000),
             title: this.state.title,
             salary: this.state.salary,
         })
         console.log('Value form: ', this.state)
+        this.setState({
+            title: '',
+            salary: '',
+        })
     }
     render() {
 
         return (
             <>
                 <form >
-                    <label htmlFor="fname">First name:</label ><br />
+                    <label htmlFor="fname">Job's title:</label ><br />
                     <input
                         type="text"
                         value={this.state.title}
                         onChange={(event) => this.handleChangeJobName(event)} /><br />
-                    <label htmlFor="lname">Last name:</label><br />
+                    <label htmlFor="lname">Salary:</label><br />
                     <input
                         type="text"
                         value={this.state.salary}
